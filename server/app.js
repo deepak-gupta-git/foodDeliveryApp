3d55af7aabb38fc64a7e368063ce376305344c10
Deepak Gupta <154800280+deepak-gupta-git@users.noreply.github.com>
2024-10-04 22:44:43 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const Razorpay = require("razorpay");
 
 const corsOptions = {
-    origin: ["http://localhost:3000", "https://food-delivery-app-frontend-0.vercel.app"],
+    origin: ["https://food-delivery-app-frontend-0.vercel.app"],
     methods: "GET, POST, PUT, PATCH, DELETE, HEAD",
     credentials: true,
 };
