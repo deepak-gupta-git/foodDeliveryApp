1d358235aac6f08db9a4b791404b8d1cae32dba4
Deepak Gupta <154800280+deepak-gupta-git@users.noreply.github.com>
2024-10-22 09:50:47 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
 
   const {storeTokenInLS} = useAuth();
 
-  const LOGINURL = "https://food-delivery-app-tgyc.vercel.app/login"
+  const LOGINURL = "​https://food-delivery-app-tgyc.vercel.app/api/auth/login"
   
 
   const handleInput = (e) => {
